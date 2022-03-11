104220a6f1ff54c75524d6d9476d314768ea079f
flex-akin <akintolafelix2121@gmail.com>
2022-03-10 19:23:35 -0800

    last task commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,7 +17,7 @@ int main(void)
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));	
-	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(g));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(g));
 	return (0);
 }
 
