1b25c97fbefdf2211b4467a0d511a3b2fbd5f7f0
flex-akin <akintolafelix2121@gmail.com>
2022-03-10 21:19:50 -0800

    final commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -Entry point
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	write(STDOUT_FILENO,"and that place of art is useful \" - Dore Korpar, 2015-10-19\n", 59);
+	return (1);
+}
