0bdc38d6ee38cb2dcde6e2f578266ded089dd91b
flex-akin <akintolafelix2121@gmail.com>
2022-03-16 01:41:46 +0000

    second commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_alphabet: check description
+ * print_alphabet - Check description
  * Description: it prints the alphabet in a lower case followed by a new line
- * Return: nothing.
+ * Return: Nothing.
  */
 void print_alphabet(void)
 {
