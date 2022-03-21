ad4a3c7e9ab85eed569d80d10afddbb5aaaee959
flex-akin <akintolafelix2121@gmail.com>
2022-03-21 23:16:40 +0000

    second task commmit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- * swap - main
- * @n: - swap integers
+ * swap_int - main
+ * @a: - swap_int
+ * @b: - second int
  */
 
 void swap_int(int *a, int *b)
 {
 	int temp;
+
 	temp = *a;
 	*a = *b;
 	*b = temp;
